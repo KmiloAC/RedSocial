21b6a0184a7df6cf32bc34d4f99ae17d2354c28f
kngarcia <159200143+kngarcia@users.noreply.github.com>
2024-11-10 20:33:05 -0500

    Update app.py

--- gateway/app.py
+++ gateway/app.py
@@ -1,31 +1,10 @@
 # gateway/app.py
-from flask import Flask, request, jsonify
-import requests
-from config import MICROSERVICIO_USUARIOS
+from flask import Flask
+from routes import gateway  # Importa el Blueprint desde routes.py
 
 app = Flask(__name__)
-
-# Ruta de prueba para redirigir a un microservicio
-@app.route('/usuarios/<path:path>', methods=['GET', 'POST', 'PUT', 'DELETE'])
-def handle_usuarios(path):
-    url = f"{MICROSERVICIO_USUARIOS}/{path}"
-    
-    # Configura los parámetros para la solicitud al microservicio
-    if request.method in ['POST', 'PUT']:
-        # Solo envía JSON en solicitudes POST o PUT
-        response = requests.request(
-            method=request.method,
-            url=url,
-            json=request.get_json()  # Enviar JSON solo en caso de POST o PUT
-        )
-    else:
-        # Para GET y DELETE, no envíes JSON
-        response = requests.request(
-            method=request.method,
-            url=url
-        )
-    
-    return jsonify(response.json()), response.status_code
+app.register_blueprint(gateway)  # Registra el Blueprint
 
 if __name__ == '__main__':
+    # Ejecuta el API Gateway en el puerto 5000
     app.run(port=5000)
